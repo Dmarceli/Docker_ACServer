155b354754fabef985753be291d4a9d9d6e345b7
felix <felix.portier@epitech.eu>
2023-05-10 00:11:49 +0200

    remove unused dep

--- srcs/conf/conf.sh
+++ srcs/conf/conf.sh
@@ -7,7 +7,7 @@ wget https://packages.microsoft.com/config/ubuntu/22.04/prod.list
 mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 chown root:root /etc/apt/sources.list.d/microsoft-prod.list
-apt-get update && apt-get install -y aspnetcore-runtime-7.0 dotnet-runtime-7.0 dotnet-sdk-7.0
+apt-get update && apt-get install -y dotnet-runtime-7.0 dotnet-sdk-7.0
 apt-get upgrade -y
 
 cd server

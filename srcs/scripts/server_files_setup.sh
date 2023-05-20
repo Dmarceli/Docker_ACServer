979f3ef33d9e35c4047290fd43d67133ca4723d5
felix <felix.portier@epitech.eu>
2023-05-20 16:18:39 +0200

    fix

--- srcs/scripts/server_files_setup.sh
+++ srcs/scripts/server_files_setup.sh
@@ -11,8 +11,10 @@ rm  ../server_files/temp/*
 echo 'unzipping server files into temp'
 unzip $server_file_name ../server_files/temp/
 echo 'Copying entry list to cfg...'
-cp -r ../server_files/temp/cfg/entry_list.ini ../tools/server/cfg/
+cp  ../server_files/temp/cfg/entry_list.ini ../tools/server/cfg/
 echo 'Copying server details to cfg...'
-cp -r ../server_files/temp/cfg/server_cfg.ini ../tools/server/cfg/
+cp  ../server_files/temp/cfg/server_cfg.ini ../tools/server/cfg/
+echo 'Copying csp_params to cfg...'
+cp  ../server_files/temp/cfg/csp_extra_options.ini ../tools/server/cfg/
 echo 'Copying mods to server'
 cp -rf ../server_files/temp/content/* ../tools/server/content/
